1deef1dab723a9c780a5570c86fd64e974e8e678
Veselin <veskotoyotov@gmail.com>
2020-05-04 23:20:40 +0300

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo ini_get("default_socket_timeout");
-?>
-<form method="post" action="Crawl.php">
-<input type="text" name="ip" placeholder="ip">
-<input type="text" name="socket" placeholder="socket">
-<button type="submit">davai</button>
-</form>
